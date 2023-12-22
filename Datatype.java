7ec8c3ca5df82318942d04d50b2e888bc29e549c
Tushar9678 <126349113+Tushar9678@users.noreply.github.com>
2023-12-22 19:31:42 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,33 @@
+public class Datatype {
+    public static void main(String[] args) {
+        // Integer DT
+        byte myByte = 10;
+        short myShort = 100;
+        int myint = 1000;
+        long myLong = 10000L;
+
+        // floating DT
+        float myFloat = 3.14f;
+        double myDouble = 5.12345;
+
+        // char DT
+        char myChar = 'T';
+
+        // boolean DT
+        boolean myBool = true;
+
+        // String data type
+        String myString = "Hello, Tushar !";
+
+        // Printing the values
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Int value: " + myint);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Character value: " + myChar);
+        System.out.println("Boolean value: " + myBool);
+        System.out.println("String value: " + myString);
+    }
+}
