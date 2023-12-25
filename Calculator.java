aedd35896d52827261120f1a80a82b1c2236b3e1
Tushar_9678 <126349113+Tushar9678@users.noreply.github.com>
2023-12-25 18:31:51 +0530

    Day5_Assignment_Done

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int operator;
+
+        System.out.println("-------------MENU-------------");
+        System.out.println("1-ADD \n2-SUBTRACT \n3-MULTIPLY  \n4-DIVISION");
+         System.out.println("Choose Operator");
+         operator =in.nextInt();
+
+
+        System.out.println("Enter a first number :");
+        int num1 = in.nextInt();
+        System.out.println("Enter a Second number :");
+        int num2 = in.nextInt();
+
+     
+        int result =0;
+        switch (operator){
+            case 1:
+            result = num1+num2;
+            break;
+
+            case 2: 
+            result = num1-num2;
+            break;
+
+            case 3:
+            result = num1*num2;
+            break;
+
+            case 4:
+            result = num1/num2;
+            break;
+
+
+        }
+        System.out.println("Ans :"+result);
+
+    }
+}
